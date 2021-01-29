993ac4b40531ce7a2e65748be1b7769a0dfd4414
Wei Jann Wong <wjannw@gmail.com>
2021-01-29 14:19:54 +0000

    refactor: Updated indentation in code for ease of review

--- part_2.sql
+++ part_2.sql
@@ -1,7 +1,8 @@
 -- Create a table for users
 CREATE​ ​TABLE​ ​"users"​(
 "user_id"​ SERIAL PRIMARY ​KEY​,
-"username"​ VARCHAR(​25​) U​ NIQUE​ ​NOT​ NULL ​CHECK​ (​LENGTH​(​TRIM​(​"username"​))>​0​), "last_login"​ ​TIMESTAMP
+"username"​ VARCHAR(​25​) U​ NIQUE​ ​NOT​ NULL ​CHECK​ (​LENGTH​(​TRIM​(​"username"​))>​0​),
+"last_login"​ ​TIMESTAMP
 );
 
 -- Create index on username in table users
@@ -20,7 +21,9 @@ CREATE​ ​INDEX​ ​ON​ ​"topics"​(​"topic_name"​);
 -- Create table for posts:
 CREATE​ ​TABLE​ ​"posts"​(
 "post_id"​ SERIAL PRIMARY ​KEY​,
-"post_title"​ VARCHAR(​100​) ​NOT​ NULL ​CHECK​ (​LENGTH​(​TRIM​(​"post_title"​))>​0​), "topic_id"​ INT ​REFERENCES​ "​ topics"​(​"topic_id"​) ​ON​ ​DELETE​ C​ ASCADE​, "user_id"​ INT ​REFERENCES​ ​"users"​(​"user_id"​) ​ON​ ​DELETE​ S​ ET​ NULL,
+"post_title"​ VARCHAR(​100​) ​NOT​ NULL ​CHECK​ (​LENGTH​(​TRIM​(​"post_title"​))>​0​),
+"topic_id"​ INT ​REFERENCES​ "​ topics"​(​"topic_id"​) ​ON​ ​DELETE​ C​ASCADE​,
+"user_id"​ INT ​REFERENCES​ ​"users"​(​"user_id"​) ​ON​ ​DELETE​ S​ ET​ NULL,
 "url"​ VARCHAR(​2000​),
 "text_content"​ TEXT,
 CONSTRAINT​ url_text ​CHECK​(("​ url"​ ​IS​ NULL ​AND​ ​"text_content"​ I​ S​ ​NOT​ NULL) ​OR
@@ -34,12 +37,16 @@ CREATE​ ​TABLE​ ​"comments"​(
 "comment_id"​ SERIAL PRIMARY ​KEY​,
 "parent_comment_id"​ INT ​REFERENCES​ ​"comments"​(​"comment_id"​) ​ON​ ​DELETE
 CASCADE​,
-"post_id"​ INT ​REFERENCES​ ​"posts"​(​"post_id"​) ​ON​ ​DELETE​ C​ ASCADE​, "user_id"​ INT ​REFERENCES​ ​"users"​(​"user_id"​) ​ON​ ​DELETE​ S​ ET​ NULL, "text"​ TEXT ​NOT​ NULL
+"post_id"​ INT ​REFERENCES​ ​"posts"​(​"post_id"​) ​ON​ ​DELETE​ C​ASCADE​,
+"user_id"​ INT ​REFERENCES​ ​"users"​(​"user_id"​) ​ON​ ​DELETE​ S​ET​ NULL,
+"text"​ TEXT ​NOT​ NULL
 );
 
 -- Create table for votes:
 CREATE​ ​TABLE​ ​"votes"​(
 "vote_id"​ SERIAL PRIMARY ​KEY​,
-"post_id"​ INT ​REFERENCES​ ​"posts"​(​"post_id"​) ​ON​ ​DELETE​ C​ ASCADE​, "user_id"​ INT ​REFERENCES​ ​"users"​(​"user_id"​) ​ON​ ​DELETE​ S​ ET​ NULL, "vote"​ SMALLINT ​CHECK​("​ vote"​ = ​1​ ​OR​ ​"vote"​ = ​-1​),
+"post_id"​ INT ​REFERENCES​ ​"posts"​(​"post_id"​) ​ON​ ​DELETE​ C​ASCADE​,
+"user_id"​ INT ​REFERENCES​ ​"users"​(​"user_id"​) ​ON​ ​DELETE​ S​ET​ NULL,
+"vote"​ SMALLINT ​CHECK​("​ vote"​ = ​1​ ​OR​ ​"vote"​ = ​-1​),
 CONSTRAINT​ ​"user_vote"​ ​UNIQUE​ (​"user_id"​, ​"post_id"​)
 );
